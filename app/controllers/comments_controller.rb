fd5e8bd82877e726e2279786d5133ce10022de9c
ejnava <ejnava@unm.edu>
2012-09-28 12:19:01 -0600

    subsequent commit with authentication 28SEP12 1220

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,7 +40,8 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(params[:comment])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(params[:comment])
 
     respond_to do |format|
       if @comment.save
