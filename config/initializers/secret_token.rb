bec2d6505eb3b73a2becd3b4f1e7e073ca5fd320
ejnava <ejnava@unm.edu>
2012-09-28 10:42:53 -0600

    intermediate commit 28SEP12 1044

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '03a016a116b84bd748a6d74ebdcdcecc1b3d76516225c93afd8684685ac55b1fbc83fe2a1359bd10708531a5ff69a6b7e4a258c39d45a04dbb12c895343c55e5'
